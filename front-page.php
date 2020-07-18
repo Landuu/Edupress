b592686722d50d7bb7b33c9d3e49a4ea9e670f50
Landu <landupcmr@gmail.com>
2020-07-18 13:11:10 +0200

    ADD: header, slider, blog

--- front-page.php
+++ front-page.php
@@ -1,16 +1,72 @@
 <?php get_header(); ?>
 
 
-<div class="container pt-5">
-    <h1>
-        <?php the_title(); ?>
-    </h1>
+<section class="slider">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="carousel-filter"></div>
+                <img src="https://www.skanska.pl/globalassets/externalcontent2/project/portland-public-schools-franklin-high-school-renovation/9f1146bd-a02a-4174-a4f0-86c6df2f46fb.1.jpg?download" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-inline-flex">
+                    <div class="caption-title display-4 py-5 text-right flex-center-vert">
+                        Zapoznaj się z zasadami rekrutacji do naszej szkoły!
+                    </div>
+                    <div class="vl"></div>
+                    <p class="caption-content py-5 flex-center-vert text-left">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec faucibus euismod condimentum. 
+                        Donec pharetra lorem sit amet ligula semper dignissim a in ipsum. 
+                        Duis placerat ante vitae orci aliquet congue. 
+                        Sed ultricies urna volutpat justo vehicula, ut varius dolor vestibulum. 
+                    </p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="carousel-filter"></div>
+                <img src="https://via.placeholder.com/1920x1080.jpg?text=Drugi+slide" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="carousel-filter"></div>
+                <img src="https://via.placeholder.com/1920x1080.jpg?text=Trzeci+slide" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</section>
 
-    <button class="btn btn-primary">Test</button>
+<section class="blog mt-5">
+    <div class="container">
+        <div class="row mb-4">
+            <div class="col text-center">
+                <div class="display-4">
+                    Najnowsze aktualności
+                </div>
+                <hr>
+            </div>
+        </div>
+    </div>
+</section>
 
-    <?php get_template_part('includes/section', 'content'); ?>
-</div>
 
-
+<!-- <?php get_template_part('includes/section', 'content'); ?> -->
 
 <?php get_footer(); ?>
--- header.php
+++ header.php
@@ -11,12 +11,61 @@
 
 <body>
 
-    <header class="sticky-top">
+    <header>
 
-        <nav class="navbar  navbar-expand-lg navbar-light bg-light ">
+        <div class="top-nav d-none d-md-block">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-auto d-inline-flex">
+                        <div class="d-inline-flex flex-center-vert">
+                            <box-icon class="icon" name='phone-call' type='solid' color='#ffffff'></box-icon>
+                            <span class="contact-details ml-1">505 404 323</span>
+                        </div>
+                        <div class="vl"></div>
+                        <div class="d-inline-flex flex-center-vert">
+                            <box-icon class="icon" name='envelope' type='solid' color='#ffffff'></box-icon>
+                            <span class="contact-details ml-1">kontakt@example.com</span>
+                        </div>
+                    </div>
+                    <div class="col-auto d-inline-flex ml-auto">
+                        <div class="d-inline-flex flex-center-vert">
+                            <box-icon class="icon" name='search-alt-2' color='#ffffff'></box-icon>
+                        </div>
+                        <div class="vl"></div>
+                        <div class="d-inline-flex flex-center-vert">
+                            <box-icon class="icon" name='facebook-square' type='logo' color='#ffffff' ></box-icon>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="logo-bar">
             <div class="container">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse-1" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="row">
+                    <div class="col d-inline-flex flex-center-vert">
+                        <?php
+                            $custom_logo_id = get_theme_mod('custom_logo');
+                            $custom_logo_url = wp_get_attachment_image_url( $custom_logo_id , 'full' );
+                            echo '<img src="' . esc_url( $custom_logo_url ) . '" alt="">';
+                        ?>
+                        <div class="sitename ml-3">
+                            <?php
+                                $site_name  = get_bloginfo('name');
+                                echo htmlspecialchars_decode($site_name);
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-auto ml-auto flex-center d-none d-md-flex">
+                        <button class="btn btn-primary btn-dziennik">E-DZIENNIK</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+            <div class="container">
+                <button class="navbar-toggler mx-auto" type="button" data-toggle="collapse" data-target="#bs-navbar-collapse-1" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <?php
@@ -26,7 +75,7 @@
                         'container'       => 'div',
                         'container_class' => 'collapse navbar-collapse',
                         'container_id'    => 'bs-navbar-collapse-1',
-                        'menu_class'      => 'navbar-nav ml-auto',
+                        'menu_class'      => 'navbar-nav mx-auto',
                         'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
                         'walker'          =>  new WP_Bootstrap_Navwalker(),
                     ]);
